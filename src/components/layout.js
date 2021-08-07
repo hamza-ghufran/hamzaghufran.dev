eb800814cb02612f61596c566138060ca61db561
Hamza Ghufran <hamzazebghufran@gmail.com>
2021-08-07 21:05:08 +0530

    init repo

---
+++ src/components/layout.js
@@ -0,0 +1,32 @@
+import * as React from "react"
+import { Link } from "gatsby"
+
+const Layout = ({ location, title, children }) => {
+  const rootPath = `${__PATH_PREFIX__}/`
+  const isRootPath = location.pathname === rootPath
+  let header
+
+  if (isRootPath) {
+    header = (
+      <h6 style={{ fontSize: "1.98818rem" }} className="main-heading">
+        <Link to="https://www.youtube.com/watch?v=e3-5YC_oHjE">{title}</Link>
+      </h6>
+    )
+  } else {
+    header = (
+      <Link className="header-link-home" to="/">
+        {title}
+      </Link>
+    )
+  }
+
+  return (
+    <div className="global-wrapper" data-is-root-path={isRootPath}>
+      <header className="global-header">{header}</header>
+      <main>{children}</main>
+      <footer></footer>
+    </div>
+  )
+}
+
+export default Layout
